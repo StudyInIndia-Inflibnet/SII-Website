8ece4cbaa64a06b34d7423a462f672c04deec691
HP <HP@DESKTOP-3KNG46H>
2020-06-18 20:12:40 +0530

    18-06-2020

--- SII/Controllers/blogsController.cs
+++ SII/Controllers/blogsController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using SIIRepository.Adminservice;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -19,6 +21,46 @@ namespace SII.Controllers
         }
         public ActionResult Page(string id = "")
         {
+            if (id == "")
+            {
+                return Redirect("~/blogs");
+            }
+            else
+            {
+                try
+                {
+                    BlogPost_Repository _obRepo = new BlogPost_Repository();
+                    DataSet _ds = _obRepo.SELECT_BLOGPOST_BY_UNIQUECODE(id);
+                    if (_ds != null)
+                    {
+                        foreach (DataRow _dr in _ds.Tables[0].Rows)
+                        {
+                            ViewBag.UniqueCode = _dr["UniqueCode"].ToString();
+                            ViewBag.BlogTitle = _dr["BlogTitle"].ToString();
+                            ViewBag.BlogContent = _dr["BlogContent"].ToString();
+                            ViewBag.BlogImage = _dr["BlogImage"].ToString();
+
+                            ViewBag.BlogMetaTags = _dr["BlogMetaTags"].ToString();
+                            ViewBag.BlogMetaTags = _dr["BlogMetaTags"].ToString();
+                            ViewBag.BlogMetaDescription = _dr["BlogMetaDescription"].ToString();
+                            ViewBag.CreatedDate = Convert.ToDateTime(_dr["CreatedDate"].ToString()).ToString("dd-MMM-yy");
+                            ViewBag.CreatedTime = Convert.ToDateTime(_dr["CreatedDate"].ToString()).ToString("hh:mm tt");
+
+                            string[] BlogTag;
+                            BlogTag = _dr["BlogTag"].ToString().Split(',');
+                            ViewBag.BlogTag = BlogTag;
+                        }
+                    }
+                    else
+                    {
+                        return Redirect("~/blogs");
+                    }
+                }
+                catch (Exception)
+                {
+                    return Redirect("~/blogs");
+                }
+            }
             return View();
         }
     }
